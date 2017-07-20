bcf7461f043dcb5fae83f60b4213bb3c1c37527e
Keita <kitswd11@gmail.com>
2017-07-21 01:05:00 +0900

    no message

--- keyboards/ergodox/keymaps/myKeymap/keymap.c
+++ keyboards/ergodox/keymaps/myKeymap/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,    KC_Y,   KC_U,    KC_I,   KC_O,    KC_P,            KC_EQL,
                   KC_H,   KC_J,    KC_K,   KC_L,    KC_SCLN,         KC_MINS,
         KC_NO,    KC_N,   KC_M,    KC_COMM,KC_DOT,  KC_SLSH,         KC_BSLS,
-                        LT(VIM, TG(VIM)), KC_RCTL,KC_RALT,KC_RSFT,   KC_ESC,
+                        LT(VIM, TG(VIM)), KC_LCTRL,KC_RALT,KC_RSFT,   KC_ESC,
         KC_NO,     KC_NO,
         KC_NO ,
         LT(MDIA,M_CONVERSION),     MT(KC_RALT, KC_TAB), MT(KC_RSFT, KC_SPC)
@@ -173,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_TRNS, KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,  KC_F12,
-       KC_TRNS, KC_NO,   KC_CTRL|KC_Z,TG(BASE),KC_NO,   KC_NO,  KC_F12,
+       KC_TRNS, KC_NO,   KC_LCTRL|KC_Z,TG(BASE),KC_NO,   KC_NO,  KC_F12,
                 KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT,   KC_NO,  KC_TRNS,
        KC_TRNS, KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,  KC_TRNS,
                          KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
