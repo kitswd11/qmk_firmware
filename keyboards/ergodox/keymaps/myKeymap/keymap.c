a90f1f0d81cc3ab0967c2af24e405885bd76f312
Keita <kitswd11@gmail.com>
2017-07-21 00:34:26 +0900

    修正

--- keyboards/ergodox/keymaps/myKeymap/keymap.c
+++ keyboards/ergodox/keymaps/myKeymap/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,    KC_6,   KC_7,    KC_8,   KC_9,    KC_0,            KC_GRV,
         KC_NO,    KC_Y,   KC_U,    KC_I,   KC_O,    KC_P,            KC_EQL,
                   KC_H,   KC_J,    KC_K,   KC_L,    KC_SCLN,         KC_MINS,
-        LT(MEDIA), KC_N,   KC_M,    KC_COMM,KC_DOT,  KC_SLSH,         KC_BSLS,
+        LT(MDIA), KC_N,   KC_M,    KC_COMM,KC_DOT,  KC_SLSH,         KC_BSLS,
                         LT(VIM, TG(VIM)), KC_RCTL,KC_RALT,KC_RSFT,   KC_ESC,
         KC_NO,     KC_NO,
         KC_NO ,
